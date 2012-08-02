6749ebdd12dd7b2fa700dbd053aaf15888ed9386
Roberto Torella <robertot@square-enix.com>
2012-08-02 12:40:38 +0100

    Added clouds, sun, moon and a fat gitignore to remove sass-cache

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
+	// Create clouds
+	var maxClouds=4;
+	var minCloudWidth = 100;
+	var maxCloudWidth = 300;
+	var minCloudHeight = 50;
+	var maxCloudHeight = 100;
+	var minCloudTop = 20;
+	var maxCloudTop = 400;
+
+	for (var i=0; i<maxClouds; i++) {
+		var $cloud = $("<div />");
+		$cloud.addClass("clouds cloud-"+Math.round(Math.random()*9+1));
+		$cloud.css({
+			width: Math.round(Math.random()*(maxCloudWidth-minCloudWidth)+minCloudWidth),
+			height: Math.round(Math.random()*(maxCloudHeight-minCloudHeight)+minCloudHeight),
+			top:Math.round(Math.random()*(maxCloudTop-minCloudTop)+minCloudTop),
+		});
+		$("body").append($cloud);
+	}
+
 	var jump = function(){
 		$(".box").addClass("jump");
 		var wait = window.setTimeout(function(){
 				$('.box').removeClass("jump")
 		},500);
 	};
-	$(document).click(jump).keydown(jump);
+	$(document).click(jump).keydown(jump).bind('touchstart',jump);
 });
